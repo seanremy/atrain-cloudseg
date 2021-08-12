b7587a4ab88a616c123dd588a1f76161fdb19401
Sean <seanremy@gmail.com>
2021-08-12 13:40:00 -0400

    add needed solar geometry flags, add field explanation comments

--- src/utils/parasol_fields.py
+++ src/utils/parasol_fields.py
@@ -2,41 +2,58 @@
 
 # All available fields
 ALL = [
-    ("Data_Directional_Fields", "I443NP"),
-    ("Data_Directional_Fields", "I490P"),
-    ("Data_Directional_Fields", "Q490P"),
-    ("Data_Directional_Fields", "U490P"),
-    ("Data_Directional_Fields", "I565NP"),
-    ("Data_Directional_Fields", "I670P"),
-    ("Data_Directional_Fields", "Q670P"),
-    ("Data_Directional_Fields", "U670P"),
-    ("Data_Directional_Fields", "I763NP"),
-    ("Data_Directional_Fields", "I765NP"),
-    ("Data_Directional_Fields", "I865P"),
-    ("Data_Directional_Fields", "Q865P"),
-    ("Data_Directional_Fields", "U865P"),
-    ("Data_Directional_Fields", "I910NP"),
-    ("Data_Directional_Fields", "I1020NP"),
-    ("Data_Fields", "NViews"),
-    ("Data_Fields", "cloud_indicator"),
-    ("Data_Fields", "phis"),
-    ("Geolocation_Fields", "Latitude"),
-    ("Geolocation_Fields", "Longitude"),
-    ("Geolocation_Fields", "land_sea_flag"),
-    ("Geolocation_Fields", "surface_altitude"),
+    ("Data_Directional_Fields", "I443NP"),  # normalized radiance for channel 443NP
+    ("Data_Directional_Fields", "I490P"),  # normalized radiance for channel 490P
+    ("Data_Directional_Fields", "Q490P"),  # second component of Stokes Vector (Q) for channel 490P
+    ("Data_Directional_Fields", "U490P"),  # third component of Stokes Vector (U) for channel 490P
+    ("Data_Directional_Fields", "I565NP"),  # normalized radiance for channel 565NP
+    ("Data_Directional_Fields", "I670P"),  # normalized radiance for channel 670P
+    ("Data_Directional_Fields", "Q670P"),  # second component of Stokes Vector (Q) for channel 670P
+    ("Data_Directional_Fields", "U670P"),  # third component of Stokes Vector (U) for channel 670P
+    ("Data_Directional_Fields", "I763NP"),  # normalized radiance for channel 763NP
+    ("Data_Directional_Fields", "I765NP"),  # normalized radiance for channel 765NP
+    ("Data_Directional_Fields", "I865P"),  # normalized radiance for channel 865P
+    ("Data_Directional_Fields", "Q865P"),  # second component of Stokes Vector (Q) for channel 865P
+    ("Data_Directional_Fields", "U865P"),  # third component of Stokes Vector (U) for channel 865P
+    ("Data_Directional_Fields", "I910NP"),  # normalized radiance for channel 910NP
+    ("Data_Directional_Fields", "I1020NP"),  # normalized radiance for channel 1020NP
+    ("Data_Directional_Fields", "phi"),  # relative azimuth angle of the pixel center  for filter 670P2
+    ("Data_Directional_Fields", "thetas"),  # solar zenith angle of the pixel center
+    ("Data_Directional_Fields", "thetav"),  # view zenith angle of the pixel center  for filter 670P2
+    ("Data_Fields", "Nviews"),  # number of available view directions [1-16]
+    ("Data_Fields", "cloud_indicator"),  # rough cloud indicator: clear (0), cloudy (100), undetermined (50)
+    ("Data_Fields", "phis"),  # solar azimuth angle of the pixel center
+    ("Geolocation_Fields", "Latitude"),  # pixel center latitude
+    ("Geolocation_Fields", "Longitude"),  # pixel center longitude
+    ("Geolocation_Fields", "land_sea_flag"),  # land sea flag (0:sea, 100:land, 50:mixed)
+    ("Geolocation_Fields", "surface_altitude"),  # pixel center surface altitude
+    # Potential error in the attitude data (0:0.01; 1:0.05; 2:0.1; 3:0.15; 4:0.25; 5:0.50; 6:1; 7: >1 ).
     ("Quality_Flags_Directional_Fields", "Quality_Flags_01"),
+    # Anomaly in the correction for optic polarization.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_02"),
+    # Pixel saturated/lacking in the 4x4 window used for bicubic interpolation.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_03"),
+    # Pixel saturated/lacking in the 4x4 window used for bicubic interpolation.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_04"),
+    # Pixel saturated/lacking in the 4x4 window used for bicubic interpolation.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_05"),
+    # Pixel saturated/lacking in the 4x4 window used for bicubic interpolation.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_06"),
+    # CCD pixel may be degraded (matrix border).
     ("Quality_Flags_Directional_Fields", "Quality_Flags_07"),
+    # CCD pixel may be degraded (matrix border).
     ("Quality_Flags_Directional_Fields", "Quality_Flags_08"),
+    # CCD pixel may be degraded (matrix border).
     ("Quality_Flags_Directional_Fields", "Quality_Flags_09"),
+    # CCD pixel may be degraded (matrix border).
     ("Quality_Flags_Directional_Fields", "Quality_Flags_10"),
+    # Stray light correction (type 1) greater than a threshold. The thresholds are defined by the ocean color mission requirements.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_11"),
+    # Stray light correction (type 1) greater than a threshold. The thresholds are defined by the other mission requirements.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_12"),
+    # Stray light correction (type 2) greater than a threshold. The thresholds are defined by the ocean color mission requirements.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_13"),
+    # Stray light correction (type 2) greater than a threshold. The thresholds are defined by the other mission requirements.
     ("Quality_Flags_Directional_Fields", "Quality_Flags_14"),
 ]
 
@@ -57,6 +74,9 @@ DEFAULT = [
     ("Data_Directional_Fields", "U865P"),
     ("Data_Directional_Fields", "I910NP"),
     ("Data_Directional_Fields", "I1020NP"),
+    ("Data_Directional_Fields", "phi"),
+    ("Data_Directional_Fields", "thetas"),
+    ("Data_Directional_Fields", "thetav"),
     ("Data_Fields", "Nviews"),
     ("Data_Fields", "cloud_indicator"),
     ("Geolocation_Fields", "Latitude"),
