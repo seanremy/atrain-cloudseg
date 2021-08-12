b00076e6945230d933b95a20eb0f040e5903e05b
Sean <seanremy@gmail.com>
2021-08-12 13:38:19 -0400

    make mid-layer depth configurable

--- src/models/single_pixel.py
+++ src/models/single_pixel.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 class SinglePixel(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int, num_layers: int = 3) -> None:
+    def __init__(self, in_channels: int, out_channels: int, num_layers: int = 3, mid_layer_depth=None) -> None:
         """Create a Single Pixel model.
 
         Args:
@@ -15,13 +15,21 @@ class SinglePixel(nn.Module):
         assert num_layers >= 1
         self.in_channels = in_channels
         self.out_channels = out_channels
+        if mid_layer_depth is None:
+            self.mid_layer_depth = max(self.in_channels, self.out_channels)
+        else:
+            self.mid_layer_depth = mid_layer_depth
         self.num_layers = num_layers
-        self.layers = []
-        for _ in range(self.num_layers - 1):
-            self.layers.append(nn.Conv2d(self.in_channels, self.in_channels, kernel_size=1))
-            self.layers.append(nn.BatchNorm2d(self.in_channels))
+        self.layers = [
+            nn.Conv2d(self.in_channels, self.mid_layer_depth, kernel_size=1),
+            nn.BatchNorm2d(self.mid_layer_depth),
+            nn.ReLU(),
+        ]
+        for _ in range(self.num_layers - 2):
+            self.layers.append(nn.Conv2d(self.mid_layer_depth, self.mid_layer_depth, kernel_size=1))
+            self.layers.append(nn.BatchNorm2d(self.mid_layer_depth))
             self.layers.append(nn.ReLU())
-        self.layers.append(nn.Conv2d(self.in_channels, self.out_channels, kernel_size=1))
+        self.layers.append(nn.Conv2d(self.mid_layer_depth, self.out_channels, kernel_size=1))
         self.layers = nn.Sequential(*self.layers)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
